0701e290fa22ceb7fa3f62bede78d29a909400c5
Yilder Navarro <80337949+Yilder13@users.noreply.github.com>
2022-06-27 21:51:55 -0500

    exercise scope and clousure

---
+++ src/scope/function.js
@@ -0,0 +1,11 @@
+function greeting() {
+    let userName = 'Ana';
+    console.log(userName);
+
+    if (userName === 'Ana') {
+        console.log(`Hello ${useName}!`)
+    }
+}
+
+greeting();
+console.log(userName);
---
+++ src/scope/re.js
@@ -0,0 +1,36 @@
+//Declaración
+var firstName; // Undifined //declarar
+firstName = 'Oscar'; // asignar
+console.log(firstName);
+
+//Asignación
+var lastName = "David"; // declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+//Reasignación
+var secondName = 'David'; //declarando /asignadno
+var secondName = 'Ana';//reasignando
+console.log(secondName);
+
+//let
+let fruit = 'Apple' // declara y asignar
+fruit = 'Kiwi'; //reasignar
+console.log(fruit);
+
+// let fruit = 'Banana';// No se puede redeclarar cla variable
+// console.log(fruit);
+
+//const
+const animal = 'dog'; // declarar y asignar
+//animal = 'cat; // reasignado / No se puede reasignar con const.
+//const animal = 'Snake'; // No se puede redeclarar la variable en const
+console.log(animal)
+
+const vehicles = [];
+vehicles.push("Carrito");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
+
